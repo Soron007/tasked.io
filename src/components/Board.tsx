ff035f3ae4c57ac831295539d0d9cddff339b6e8
Soron007 <mitrasouvik123@gmail.com>
2023-08-29 13:14:48 +0530

    implemented delete Board function

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addBoard } from "../store/kanbanSlice";
+import { addBoard, deleteBoard } from "../store/kanbanSlice";
 import { AppDispatch, RootState } from "../store/store";
+
 import Task from "./Task";
 
 const Board: React.FC = () => {
@@ -26,6 +27,10 @@ const Board: React.FC = () => {
     if (e.key === "Enter") {
       handleAddBoard();
     }
+  };
+
+  const handleDeleteBoard = (boardId: string) => {
+    dispatch(deleteBoard(boardId));
   };
 
   return (
@@ -81,9 +86,31 @@ const Board: React.FC = () => {
             } md:m-4 md:p-4 m-2 p-2 flex flex-col items-start`}
             key={board.id}
           >
-            <h2 className="text-purple-600 font-mono font-semibold md:text-xl text-lg">
-              {board.title}
-            </h2>
+            <div className="flex w-full justify-between items-center">
+              <span className="text-black font-mono font-semibold text-2xl md:text-3xl">
+                {board.title}
+              </span>
+
+              <span
+                className="mr-2"
+                onClick={() => handleDeleteBoard(board.id)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5 text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </span>
+            </div>
 
             <div className="border-2 border-gray-400 w-[80%] mt-3"></div>
 
