27367f84552aeef4f0f8e3e18058a2facfc5948d
Soron007 <mitrasouvik123@gmail.com>
2023-09-01 21:55:06 +0530

    implemented edit Tasks functionality

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-import { addTask, deleteTasks } from "../store/kanbanSlice";
-import { AppDispatch } from "../store/store";
 import { useDispatch } from "react-redux";
+import { addTask, deleteTasks, editTasks } from "../store/kanbanSlice";
+import { AppDispatch } from "../store/store";
+import { AiTwotoneDelete, AiFillEdit } from "react-icons/ai";
+import { MdCancel } from "react-icons/md";
 
 interface Task {
   id: string;
@@ -16,6 +18,8 @@ interface BoardProps {
 const Task = ({ boardId, tasks }: BoardProps) => {
   const dispatch: AppDispatch = useDispatch();
   const [taskText, setTaskText] = useState<string>("");
+  const [editText, setEditText] = useState<string>("");
+  const [editingTaskId, setEditingTaskId] = useState<string | null>(null);
 
   const handleAddTask = () => {
     if (taskText.trim() !== "") {
@@ -33,9 +37,14 @@ const Task = ({ boardId, tasks }: BoardProps) => {
     dispatch(deleteTasks({ boardId, taskId }));
   };
 
+  const handleEditTask = (taskId: string, newText: string) => {
+    dispatch(editTasks({ boardId, taskId, newText }));
+    setEditingTaskId(null);
+  };
+
   return (
     <>
-      <h2 className="font-mono text-lg font-bold mt-1">Tasks:</h2>
+      <h2 className="font-mono text-lg font-bold mt-1 mb-2">Tasks:</h2>
       <div className="flex space-x-2">
         <input
           type="text"
@@ -46,10 +55,10 @@ const Task = ({ boardId, tasks }: BoardProps) => {
         />
 
         <button
-          className="ml-2 bg-slate-300 py-1 rounded-lg hover:bg-green-600 px-3 md:inline-block hidden"
+          className="text-lg items-center ml-2 bg-slate-300 py-1 rounded-lg hover:bg-green-600 px-3 md:flex"
           onClick={handleAddTask}
         >
-          Add Task
+          +
         </button>
 
         <svg
@@ -61,11 +70,7 @@ const Task = ({ boardId, tasks }: BoardProps) => {
           className="w-8 h-8 md:hidden"
           onClick={handleAddTask}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 4.5v15m7.5-7.5h-15"
-          />
+          {/* ... */}
         </svg>
       </div>
       <div className="flex flex-col w-full">
@@ -73,25 +78,35 @@ const Task = ({ boardId, tasks }: BoardProps) => {
           {tasks.map((task) => (
             <div key={task.id}>
               <li className="mt-2 p-2 border-2 border-purple-500 flex items-center justify-between bg-slate-500/[0.6] rounded-lg duration-300 hover:scale-y-95 cursor-pointer">
-                <span className="overflow-auto font-mono font-bold text-2xl">
-                  {task.text}
-                </span>
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6 text-white"
-                    onClick={() => handleDeleteTask(task.id)}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                    />
-                  </svg>
+                {editingTaskId === task.id ? (
+                  <input
+                    type="text"
+                    value={editText}
+                    className="p-2 placeholder:text-center outline-none rounded-md"
+                    placeholder="Enter new text"
+                    onChange={(e) => setEditText(e.target.value)}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        handleEditTask(task.id, taskText);
+                      }
+                    }}
+                  />
+                ) : (
+                  <span className="overflow-auto font-mono font-bold text-2xl">
+                    {task.text}
+                  </span>
+                )}
+                <span className="flex ml-2 gap-2">
+                  {editingTaskId === task.id ? (
+                    <button onClick={() => handleEditTask(task.id, editText)}>
+                      <MdCancel />
+                    </button>
+                  ) : (
+                    <button onClick={() => setEditingTaskId(task.id)}>
+                      <AiFillEdit />
+                    </button>
+                  )}
+                  <AiTwotoneDelete onClick={() => handleDeleteTask(task.id)} />
                 </span>
               </li>
             </div>
